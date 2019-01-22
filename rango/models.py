a79f1dace23998491ed3b5c24876a96d13f71e52
Jordyn Brown <2333730B@student.gla.ac.uk>
2019-01-22 11:50:47 +0000

    6.3 this one

--- rango/models.py
+++ rango/models.py
@@ -3,31 +3,30 @@ from django.contrib import admin
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
-    slug = models.SlugField()
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-
-    class Meta:
-        verbose_name_plural = 'categories'
-
-    def __str__(self): 
-        return self.name
-
+	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	slug = models.SlugField()
+	
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+	
+	class Meta:
+		verbose_name_plural = 'Categories'
+	
+	def __str__(self):
+		return self.name
+		
 
 class Page(models.Model):
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.title
-
-
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+	
+	def __str__(self): 
+		return self.title
+		
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
--- rango/views.py
+++ rango/views.py
@@ -5,11 +5,12 @@ from rango.models import Page
 from django.http import HttpResponse
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-    return render(request, 'rango/index.html', context_dict)
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages':page_list}
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': "Rango says here is the about page"}
-    return render(request, 'rango/about.html', context=context_dict)
-
+	context_dict = {'boldmessage': "Rango says here is the about page"}
+	return render(request, 'rango/about.html', context=context_dict)
+	
