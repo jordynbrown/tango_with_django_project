1f0ecd7926f9b7938091370c8ea3e16fcc826dcc
Jordyn Brown <2333730B@student.gla.ac.uk>
2019-01-22 12:37:10 +0000

    6 finished

--- rango/views.py
+++ rango/views.py
@@ -14,3 +14,16 @@ def about(request):
 	context_dict = {'boldmessage': "Rango says here is the about page"}
 	return render(request, 'rango/about.html', context=context_dict)
 	
+def show_category(request, category_name_slug):
+	context_dict = {}
+	
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+		
+	return render(request, 'rango/category.html', context_dict)
